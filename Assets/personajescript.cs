5752df6e7ea1140f9da4e801a0676da5a7ef9a0e
RamoLLino <146351285+RamoLLino@users.noreply.github.com>
2024-06-06 10:42:23 -0300

    el ultimo
    aef

---
+++ Assets/personajescript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class personajescript : MonoBehaviour
+{
+
+    public int nivelhambre;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Tomaralimento(int valoralimentario)
+    {
+        nivelhambre -= valoralimentario;
+        if (nivelhambre <= 0)
+        {
+            nivelhambre = 0;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        alimentoscript alimento;
+        alimento = other.GetComponent<alimentoscript>();
+        if (alimento)
+        {
+            Tomaralimento(alimento.valoralimentario);
+            Destroy(other.gameObject);
+        }
+    }
+
+}
+
